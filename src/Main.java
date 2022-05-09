4f5548f9a3824857b9560f1c5547920a4bad2354
gamgae <mole4020@gmail.com>
2022-05-09 11:57:36 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+import 신규아이디추천.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        System.out.println(solution.solution("...!@BaT#*..y.abcdefghijklm"));
+    }
+}
